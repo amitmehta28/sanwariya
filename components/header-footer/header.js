4e37ee7d6d5f913163e07c05f6873e7940d4fa64
amit-mehta28 <amitmehta7973@gmail.com>
2023-07-23 19:24:02 +0530

    working in header

--- components/header-footer/header.js
+++ components/header-footer/header.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import logo from '/public/images/Logo.png'
+import searchIcon from '/public/icons/search.svg'
 
 const Header = () => {
+
+    const [sideBar, setSideBar] = useState(false);
+    const [searchBar, setSearchBar] = useState(false);
+    const [toggle, setToggle] = useState(false);
+    const [isShown, setIsShown] = useState(0);
+    const [text, setText] = useState('All Product');
+    
+    const [data, setData] = useState([]);
+
+    const [serchedData, setSerchedData] = useState();
     return (
         <>
-            <div>Header</div>
+            <header className="py-8 ">
+                <div className='container'>
+                    <div className='flex justify-between bg-white items-center'>
+                        <Image src={logo} width={276} height={40} alt='logo' />
+                        <nav>
+                            <ul className='flex gap-6'>
+                                <li><Link href="/" className='nav-info hover:text-primary hover:border-b-2 border-primary'>HOME</Link> </li>
+                                <li className='nav-info hover:text-primary flex gap-2 items-center'>
+                                    <p>ABOUT US</p>
+                                    <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1 1L4.93934 4.93934C5.52513 5.52513 6.47517 5.52483 7.06095 4.93905C8.4141 3.5859 9.42961 2.57039 11 1" stroke="#110833" stroke-width="1.5" stroke-linecap="round" />
+                                    </svg>
+                                </li>
+                                <li className='nav-info hover:text-primary flex gap-2 items-center'>
+                                    <p>PRODUCTS</p>
+                                    <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1 1L4.93934 4.93934C5.52513 5.52513 6.47517 5.52483 7.06095 4.93905C8.4141 3.5859 9.42961 2.57039 11 1" stroke="#110833" stroke-width="1.5" stroke-linecap="round" />
+                                    </svg>
+                                </li>
+                                <li><Link href="/" className='nav-info hover:text-primary'>CONTACT US</Link> </li>
+                            </ul>
+                        </nav>
+                        <div className={`relative transition-300 `}>
+                                <Image src={searchIcon} width={18} height={18} alt="Search Icon" className='cursor-pointer' onClick={() => { setSearchBar(true) }} />
+
+                              
+                            </div>
+                    </div>
+                </div>
+            </header>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,59 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xsm: '575px',
+      sm: '640px',
+      md: '768px',
+      xmd: '991px',
+      lg: '1024px',
+      xl: '1200px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary': '#AD54F2',
+        'primary-100': '#BA6EF4',
+        'primary-200': '#C687F6',
+        'primary-300': '#D2A1F8',
+        'primary-400': '#DEBBFA',
+        'primary-500': '#E8CFFB',
+        'primary-600': '#EFDDFC',
+        'primary-700': '#F5EAFD',
+        'primary-800': '#FBF6FE',
+        'primary-900': '#FDFCFF',
+
+        'secondary': '#7854F7',
+        'secondary-dark': '#9E88B2',
+
+        // 'text-main': 'linear-gradient(128.7deg, #7854F7 -287.43%, #110833 100%)',
+        'light-text': '#5D5775',
+        'main-text': '#110833',
+        'dark-text': '#282046',
+
+        'back-white': '#FAFAFA',
+        'back-black': '#0D0C0D',
+        'icon-bg': '#FFF6FF',
+
+        'divider-main': '#F2EEFE',
+        'divider-light': '#EFEEF1',
+
+        'link-main': '#1496F5',
+
+        'dark-blue': '#8a43c2'
+      },
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        'btnShadow': '0px 8px 20px rgba(120, 84, 247, 0.06)',
+        'btnShadow-Dropdown': '0px 0px 30px rgba(182, 89, 255, 0.04), 0px 8px 40px rgba(0, 0, 0, 0.06);',
+      }
     },
   },
   plugins: [],
